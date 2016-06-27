29f6008b5226e072a344d289ab77eb853ed60779
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-06-27 17:36:04 +0900

    modify file

--- materialize/js/Modals.js
+++ materialize/js/Modals.js
@@ -1,3 +1,9 @@
-
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
 $('#modal1').openModal();
 $('#modal1').closeModal();
+
+$('#modal2').openModal();
+$('#modal2').closeModal();
