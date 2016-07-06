3f481ab19841b4ccd8121ba535c0e767545447b0
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-07-06 16:33:03 +0900

    modify file

--- glean/chat/tests.py
+++ glean/chat/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ModelsTestCase(TestCase):
+    def test_Theme__str__(self):
+
+
--- glean/chat/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from glean.chat.models import Theme,Comments,ThemeRegister
-
-class TestModels(unittest.TestCase):
-
-    def test__str__(self):
-        self.assertEqual(models.__str__(),)
