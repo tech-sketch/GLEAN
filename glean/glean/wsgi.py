1f61e2c97110e132cb47f61b43f22256462ffd99
hayashizakitakaaki <ground.fleet@gmail.com>
2016-07-08 15:02:41 +0900

    [reomve]no use dir path

--- glean/glean/wsgi.py
+++ glean/glean/wsgi.py
@@ -12,24 +12,11 @@ import site
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# python virtualenv dir
-site.addsitedir("/var/www/cgi-bin/virtualenv/lib/python3.4/site-packages")
-
 sys.path.append('/var/www/cgi-bin/GLEAN')
 sys.path.append('/var/www/cgi-bin/GLEAN/glean')
 sys.path.append('/var/www/cgi-bin/GLEAN/glean/glean')
 sys.path.append('/var/www/cgi-bin/GLEAN/glean/chat')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-activate_env = os.path.expanduser("/var/www/cgi-bin/virtualenv/bin/activate_this.py")
-# execfile(activate_env, dict(__file__=activate_env))
-'''
-with open(activate_env) as f:
-    code = compile(f.read(), activate_env, 'exec')
-    exec(code, dict(__file__=activate_env))
-'''
-
-exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), dict(__file__=activate_env))
 
 application = get_wsgi_application()
