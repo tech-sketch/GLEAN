7f6982cd6cf5eda26ceaffde2a0d7c12d880a4ca
hayashizakitakaaki <ground.fleet@gmail.com>
2016-08-01 10:53:07 +0900

    [fix]add fields what id where all models

--- glean/chat/serializers.py
+++ glean/chat/serializers.py
@@ -19,21 +19,21 @@ class CommentSerializer(ModelSerializer):
     class Meta:
         model = 'chat.Comment'
         # フロントから参照することのできる情報の定義
-        publish_fields = ('comment', 'theme', 'auth', 'good', 'createdate')
+        publish_fields = ('id', 'comment', 'theme', 'auth', 'good', 'createdate')
         # フロントから更新することのできる情報の定義
         # フロントからcreateなどするときに、最低限渡す必要がある情報
-        update_fields = ('comment', 'theme', 'auth', 'good', 'createdate', )
+        update_fields = ('id', 'comment', 'theme', 'auth', 'good', 'createdate', )
 
 
 class ThemeSerializer(ModelSerializer):
     class Meta:
         model = 'chat.Theme'
-        publish_fields = ('theme', 'text', 'auth', 'createdate', 'updatedate', 'is_enforce', 'good')
-        update_fields = ('theme', 'text', 'auth', 'createdate', 'updatedate', 'is_enforce', 'good',)
+        publish_fields = ('id', 'theme', 'text', 'auth', 'createdate', 'updatedate', 'is_enforce', 'good')
+        update_fields = ('id', 'theme', 'text', 'auth', 'createdate', 'updatedate', 'is_enforce', 'good',)
 
 
 class ThemeRegisterSerializer(ModelSerializer):
     class Meta:
         model = 'chat.ThemeRegister'
-        publish_field = ('user', 'theme', 'is_read')
-        update_fields = ('user', 'theme', 'is_read', )
+        publish_field = ('id', 'user', 'theme', 'is_read')
+        update_fields = ('id', 'user', 'theme', 'is_read', )
